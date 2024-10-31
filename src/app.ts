325d424c6fb25e4e170e8c05d3cd4c36656c4882
danytulumidis <dextavision@gmail.com>
2024-10-31 13:34:06 +0100

    add cors to fetch data from the frontend

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import { json } from 'body-parser'
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 import { transitLinesRouter } from './api/transit-lines'
 
 export const app = express()
+
+app.use(
+  cors({
+    origin: 'http://localhost:4200',
+  })
+)
 
 app.use(morgan('dev'))
 
